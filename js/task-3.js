43d88b0949d66bdb6f71e5736ac8e093ce19d02a
Daryna <darina.kuritsina@gmail.com>
2024-07-23 17:52:39 +0200

    update

--- js/task-3.js
+++ js/task-3.js
@@ -8,3 +8,25 @@
 // Під час набору тексту в інпуті його поточне значення підставляється в span#name-output як ім’я для привітання
 // Значення в інпуті очищене від пробілів по краях
 // Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous"
+
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+textInput.classList.add("js-iteam-list", "js-input");
+output.classList.add("js-item-title");
+document.querySelector("h1").classList.add("js-item-title", "js-input");
+
+
+const onFieldInput = event => {
+    const username = String(textInput.value).trim();
+    if (username !== "") {
+       return output.textContent = `${username}`;  
+    } else {
+        return output.textContent = "Anonymous";
+    }
+      
+};
+
+textInput.addEventListener("input", onFieldInput);
+    
+
+
